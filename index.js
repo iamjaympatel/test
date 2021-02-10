2d813b735dbc445dfcb48028c3219f433cdb4656
iamjaympatel <jaykumarpatel657@gmail.com>
2021-02-10 10:32:16 +0530

    first commit

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.send('hello world')
 })
-const Port=process.env.Port || 5000
+//const Port=process.env.Port || 5000
 
-app.listen(Port,()=>{
+app.listen(process.env.Port || 5000,()=>{
     console.log("server start at 5000")
 })
